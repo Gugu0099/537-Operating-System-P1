da94607aaa62ee52d15e2881194777dfea2783bc
Your Name <you@example.com>
2023-01-30 20:24:25 -0600

    writing to 2d matrix failing

--- badger_fortune.c
+++ badger_fortune.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     int row = 0;
     int col = 0;
-    char dimLine[256];
+    char dimLine[100];
     // Error: invalid number of arguments
     if(argc < 5){
         printf("USAGE: \n\tbadger-fortune -f <file> -n <number> (optionally: -o <output file>) \n\t\t OR \n\tbadger-fortune -f <file> -b <batch file> (optionally: -o <output file>)\n");
@@ -28,23 +28,40 @@ int main(int argc, char *argv[]) {
     if(fp == NULL){
         printf("error");
     }
-    char currLine[100];
 
     // get dimensions of the fortune file
     int idx = 0;
     while (1 && idx < 2){
         if(idx == 0){
-            fgets(currLine, 100, fp);
-            row = atoi(currLine);
+            fgets(dimLine, 100, fp);
+            row = atoi(dimLine);
         }
         if (idx == 1){
-            fgets(currLine, 100, fp);
-            col = atoi(currLine);
+            fgets(dimLine, 100, fp);
+            col = atoi(dimLine);
         }
         idx++;
     }
-    while(fgets(currLine, 100, fp)) {
+
+    // creating space for fortune file
+    char **fortuneArr = (char **) malloc(sizeof(char*)*row);
+    for(int i = 0; i < row; i++){
+        fortuneArr[i] = (char *) malloc(sizeof(char)*col);
+    }
+    printf("%d\n", idx);
+    char currLine[col];
+    // figure out how to read everything into the f
+    while(fgets(currLine, col, fp)) {
+        if(strcmp(currLine, "%") == 0){ // ignore the % separator
+            idx++;
+            continue;
+        }
+        for(int i = 0; i < col && currLine[i] != '\0';i++){
+            fortuneArr[idx-2][i] = currLine[i];
+        }
         printf("%s\n", currLine);
+        idx++;
+        //printf("%s\n", fortuneArr[idx-2]);
     }
     /*
     // parsing fortune file into an array ie reading each command line by line
@@ -55,5 +72,11 @@ int main(int argc, char *argv[]) {
     }
     */
    fclose(fp);
+
+   // free fortune array at the end
+    for(int i = 0; i < row; i++){
+        free(fortuneArr[i]);
+    }
+    free(fortuneArr);
     return 0;
 }
